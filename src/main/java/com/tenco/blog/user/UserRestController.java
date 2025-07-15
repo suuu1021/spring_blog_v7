124272c872f7a494e5c413c37e6f0bd5a79261c7
suuu1021 <whs5758@naver.com>
2025-07-15 10:45:00 +0900

    USER 도메인 REST API 수정 처리

--- src/main/java/com/tenco/blog/user/UserRestController.java
+++ src/main/java/com/tenco/blog/user/UserRestController.java
@@ -1,13 +1,13 @@
 package com.tenco.blog.user;
 
 import com.tenco.blog._core.common.ApiUtil;
+import com.tenco.blog.utils.Define;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -26,7 +26,7 @@ public class UserRestController {
     // JSON 형식의 데이터를 추출할 때 @RequestBody 선언
     public ResponseEntity<?> join(@RequestBody UserRequest.JoinDTO reqDTO) {
         log.info("회원 가입 API 호출 - 사용자 명 : {}, 이메일 : {}",
-                            reqDTO.getUsername(), reqDTO.getEmail());
+                reqDTO.getUsername(), reqDTO.getEmail());
         reqDTO.validate();
 
         // 서비스에 위임 처리
@@ -37,10 +37,49 @@ public class UserRestController {
                 .body(new ApiUtil<>(joinUser));
     }
 
-    // 로그인 요청
+    // http://localhost:8080/login
+    // 로그인 API - (POST)
+    @PostMapping("/login")
+    public ResponseEntity<ApiUtil<UserResponse.LoginDTO>> login(
+            @RequestBody UserRequest.LoginDTO reqDTO, HttpSession session) {
+        log.info("로그인 API 호출 - 사용자명 : {}", reqDTO.getUsername());
+        reqDTO.validate();
+        UserResponse.LoginDTO loginUser = userService.login(reqDTO);
+        // 세션에 정보 저장
+        session.setAttribute(Define.SESSION_USER, loginUser);
+        return ResponseEntity.ok(new ApiUtil<>(loginUser));
+    }
+
+
     // 회원 정보 조회
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<ApiUtil<UserResponse.DetailDTO>> getUserInfo(
+            @PathVariable(name = "id") Long id, HttpSession session) {
+        log.info("회원정보 API 호출 - id : {}", id);
+        User sessionUser = (User) session.getAttribute(Define.SESSION_USER);
+        UserResponse.DetailDTO userDetail = userService.findUserById(id, sessionUser);
+        return ResponseEntity.ok(new ApiUtil<>(userDetail));
+    }
+
     // 회원 정보 수정
+    @PutMapping("/api/users/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable(name = "id")Long id,
+                                        @RequestBody UserRequest.UpdateDTO updateDTO) {
+        // 인증검사는 인터셉터에서 처리됨
+        // 유효성 검사
+        updateDTO.validate();
+        UserResponse.UpdateDTO updateUser = userService.updateById(id, updateDTO);
+        return ResponseEntity.ok(new ApiUtil<>(updateUser));
+    }
+
+
     // 로그아웃 처리
+    @GetMapping("/logout")
+    public ResponseEntity<ApiUtil<String>> logout(HttpSession session) {
+        log.info("로그아웃 API 호출");
+        session.invalidate();
+        return ResponseEntity.ok(new ApiUtil<>("로그아웃 성공"));
+    }
 
 }
 
--- src/main/java/com/tenco/blog/user/UserService.java
+++ src/main/java/com/tenco/blog/user/UserService.java
@@ -1,6 +1,7 @@
 package com.tenco.blog.user;
 
 import com.tenco.blog._core.errors.exception.Exception400;
+import com.tenco.blog._core.errors.exception.Exception403;
 import com.tenco.blog._core.errors.exception.Exception404;
 import lombok.RequiredArgsConstructor;
 
@@ -81,5 +82,19 @@ public class UserService {
 
         // 3. 응답 DTO 반환
         return new UserResponse.UpdateDTO(selectedUser);
+    }
+
+    public UserResponse.DetailDTO findUserById(Long id, User sessionUser) {
+
+        // 권한 확인
+        if(!sessionUser.getId().equals(id)) {
+            throw new Exception403("본인 정보만 조회 가능합니다");
+        }
+        // 정보 조회
+        User selectedUser = userJpaRepository.findById(id).orElseThrow(() -> {
+           throw new Exception404("사용자를 찾을 수 없습니다");
+        });
+        // 응답 DTO 변환 처리
+        return new UserResponse.DetailDTO(selectedUser);
     }
 }
